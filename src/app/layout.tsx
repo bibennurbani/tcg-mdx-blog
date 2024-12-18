dc91433a15eed725c391e57afdc02a8d3ae70591
Biben Nurbani Hasan <bibennurbani@gmail.com>
2024-12-18 08:43:28 +0700

    Enabled Theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,18 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { ThemeProvider } from '@/components/theme-provider';
 import Header from '@/components/Header';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
+    <html lang='en' suppressHydrationWarning>
       <body className={inter.className}>
-        <Header />
-        <main className='container mx-auto px-4 py-8'>{children}</main>
+        <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+          <Header />
+          <main className='container mx-auto px-4 py-8'>{children}</main>
+        </ThemeProvider>
       </body>
     </html>
   );
